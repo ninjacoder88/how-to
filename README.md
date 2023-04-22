36cb6cf1a553c9a99b5d26efb1c49ef4f1fdfba9
Tom Harris <tomharris88@outlook.com>
2023-04-22 09:00:42 -0500

    Created new web app

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # how-to
 A wide ranging project showing how to setup different technologies
+
+# to-do
+### Docker
+[ ] create docker compose
+
+### WebApi
+[ ]
+
+### Identity
+[ ] 
+
+### WebApp
+[ ] split modal into information modal and error modal
+[ ] error modal should have red background
+[ ] modals js lib should not need to have a div created (on the layout page) to be able to be used
+
+### Blazor
+[ ] 
---
+++ src/HowTo.WebApp/wwwroot/js/requireConfig.js
@@ -0,0 +1,15 @@
+﻿let appVersion = document.getElementById("app-version");
+
+requirejs.config({
+    urlArgs: appVersion.value,
+    baseUrl: "/js/lib",
+    paths: {
+        bootstrap: "bootstrap.bundle-5.1.3.min",
+        httpService: "httpService-1.0.0",
+        jquery: "jquery-3.6.3.min",
+        knockout: "knockout-3.5.0.min",
+        pubsub: "pubsub-1.0.0",
+        text: "text-2.0.16",
+        howToModal: "how-to-modal/howToModal"
+    }
+});
