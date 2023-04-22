77b12ada8f3f35698415c8269b9e7be512ce20c4
Tom Harris <tomharris88@outlook.com>
2023-04-22 09:37:30 -0500

    minor tweaks

--- src/HowTo.WebApp/Program.cs
+++ src/HowTo.WebApp/Program.cs
@@ -1,21 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddAuthentication();
+//builder.Services.AddAuthorization();
+builder.Services.AddHealthChecks();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.UseStaticFiles()
+    .UseRouting()
+    .UseHealthChecks("/health")
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
 
 app.Run();
--- src/HowTo.WebApp/wwwroot/js/app/IndexViewModel.js
+++ src/HowTo.WebApp/wwwroot/js/app/IndexViewModel.js
@@ -1,1 +1,10 @@
-﻿
+﻿require(["knockout", "httpService", "bootstrap", "pubsub", "jquery"],
+    function (ko, http, bs, ps) {
+        "use strict";
+
+        function ViewModel() {
+            const self = this;
+        }
+
+        ko.applyBindings(new ViewModel(), document.getElementById("container"));
+    });
