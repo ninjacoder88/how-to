ecb4ce6e88678ebcf114bbcc4f1ef8a016e0862e
Tom Harris <tomharris88@outlook.com>
2023-04-29 14:06:46 -0500

    renamed httpService to http

--- src/HowTo.WebApp/wwwroot/js/requireConfig.js
+++ src/HowTo.WebApp/wwwroot/js/requireConfig.js
@@ -2,14 +2,14 @@
 
 requirejs.config({
     urlArgs: appVersion.value,
-    baseUrl: "/js/lib",
+    baseUrl: "/js",
     paths: {
-        bootstrap: "bootstrap.bundle-5.1.3.min",
-        httpService: "httpService-1.0.0",
-        jquery: "jquery-3.6.3.min",
-        knockout: "knockout-3.5.0.min",
-        pubsub: "pubsub-1.0.0",
-        text: "text-2.0.16",
-        howToModal: "how-to-modal/howToModal"
+        bootstrap: "lib/bootstrap.bundle-5.1.3.min",
+        http: "lib/http-1.0.0",
+        jquery: "lib/jquery-3.6.3.min",
+        knockout: "lib/knockout-3.5.0.min",
+        pubsub: "lib/pubsub-1.0.0",
+        text: "lib/text-2.0.16",
+        howToModal: "lib/how-to-modal/howToModal"
     }
 });
