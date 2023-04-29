0caddd4d934aec7dd539d974cace5615a3bded5d
Tom Harris <tomharris88@outlook.com>
2023-04-29 14:13:51 -0500

    new page to manage events

---
+++ src/HowTo.WebApp/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HowTo.WebApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
