3c12746dc5829893bfc31b48e5e961bc15659400
Tom Harris <tomharris88@outlook.com>
2023-04-30 21:57:54 -0500

    loading events on calendar

--- src/HowTo.WebApp/wwwroot/js/app/IndexViewModel.js
+++ src/HowTo.WebApp/wwwroot/js/app/IndexViewModel.js
@@ -1,9 +1,35 @@
-﻿require(["knockout", "jquery", "bootstrap"],
-    function (ko) {
+﻿require(["knockout", "http", "pubsub", "jquery", "bootstrap"],
+    function (ko, http, ps) {
         "use strict";
+
+        function HowToEvent(obj) {
+            const self = this;
+            self.title = obj.title;
+            self.description = obj.description;
+            self.startDate = obj.startDate;
+            self.startTime = obj.startTime;
+            self.endDate = obj.endDate;
+            self.endTime = obj.endTime;
+        }
 
         function ViewModel() {
             const self = this;
+            const apiUrl = document.getElementById("api-url").value;
+
+            self.loadEvents = function () {
+                http.getAsync(`${apiUrl}api/events`)
+                    .then(events => {
+                        const array = [];
+                        events.forEach(e => {
+                            array.push(new HowToEvent(e));
+                        });
+                        ps.publish("events-loaded", { events: array });
+                    }).catch(error => {
+                        console.error(error);
+                    });
+            };
+
+            self.loadEvents();
         }
 
         ko.components.register("calendar-component", {
