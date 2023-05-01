1301a921414fbfb48c660de2d751705368da0601
Tom Harris <tomharris88@outlook.com>
2023-04-30 20:57:29 -0500

    Added create and load api actions

---
+++ src/HowTo.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,89 @@
+﻿using HowTo.DataAccess;
+using HowTo.DataAccess.Models;
+using HowTo.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace HowTo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    [RequiredScope("WebAppScope")]
+    public class EventsController : ControllerBase
+    {
+        public EventsController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string? startDate, [FromQuery] string? startTime, [FromQuery] string? endDate, [FromQuery] string? endTime)
+        {
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+
+            DateTimeOffset startDateTimeOffset = GetFromDateAndTime(startDate, startTime, today);
+            DateTimeOffset endDateTimeOffset = GetFromDateAndTime(endDate, endTime, tomorrow);
+
+            if (startDateTimeOffset >= endDateTimeOffset)
+                return BadRequest("Start must be before end");
+
+            var events = await _repository.LoadEventsAsync(startDateTimeOffset, endDateTimeOffset);
+            return new JsonResult(new ResponseModel<List<EventModel>>(true) { Data = events });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateEventModel model)
+        {
+            if (model == null)
+                return BadRequest("No data received");
+
+            if (string.IsNullOrEmpty(model.Title))
+                return BadRequest("Title must be set");
+
+            if (string.IsNullOrEmpty(model.StartDate) || string.IsNullOrEmpty(model.StartTime))
+                return BadRequest("Start date and time must be set");
+
+            if (string.IsNullOrEmpty(model.EndDate) || string.IsNullOrEmpty(model.EndTime))
+                return BadRequest("End date and time must be set");
+
+            string startDateTime = $"{model.StartDate} {model.StartTime}";
+            string endDateTime = $"{model.EndDate} {model.EndTime}";
+
+            if (!DateTimeOffset.TryParse(startDateTime, out DateTimeOffset startDateTimeOffset))
+                return BadRequest("Could not convert start date and time");
+
+            if (!DateTimeOffset.TryParse(endDateTime, out DateTimeOffset endDateTimeOffset))
+                return BadRequest("Could not convert end date and time");
+
+            if (startDateTimeOffset >= endDateTimeOffset)
+                return BadRequest("Start must be before end");
+
+            var eventId = await _repository.CreateEventAsync(model.Title, model.Description, startDateTimeOffset, endDateTimeOffset);
+            return new JsonResult(new ResponseModel<string>(true) { Data = eventId });
+        }
+
+        private static DateTimeOffset GetFromDateAndTime(string? date, string? time, DateTime today)
+        {
+            string dateTimeStr;
+
+            if (string.IsNullOrEmpty(date))
+                dateTimeStr = $"{today:yyyy-MM-dd} ";
+            else
+                dateTimeStr = $"{date} ";
+
+            if (string.IsNullOrEmpty(time))
+                dateTimeStr += $"00:00";
+            else
+                dateTimeStr += time;
+
+            if (!DateTimeOffset.TryParse(dateTimeStr, out DateTimeOffset dateTimeOffset))
+                return new DateTimeOffset(today);
+            return dateTimeOffset;
+        }
+
+        private readonly IRepository _repository;
+    }
+}
---
+++ src/HowTo.WebApi/Models/CreateEventModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HowTo.WebApi.Models
+{
+    public class CreateEventModel
+    {
+        public string StartDate { get; set; }
+
+        public string StartTime { get; set; }
+
+        public string EndDate { get; set; }
+
+        public string EndTime { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
