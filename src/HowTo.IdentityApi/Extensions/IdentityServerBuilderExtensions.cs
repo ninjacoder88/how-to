f2d6c8f4680c974acc56acdf73e3262fc1fe16e4
Tom Harris <tomharris88@outlook.com>
2023-05-01 22:36:41 -0500

    Added production certificate loading

--- src/HowTo.IdentityApi/Extensions/IdentityServerBuilderExtensions.cs
+++ src/HowTo.IdentityApi/Extensions/IdentityServerBuilderExtensions.cs
@@ -6,13 +6,50 @@ namespace HowTo.IdentityApi.Extensions
     {
         public static void AddProductionCredential(this IIdentityServerBuilder builder, IConfiguration configuration)
         {
+            if(TryFindCertificate("CertificateThumbprintPrimary", out X509Certificate2? primaryCertificate))
+            {
+                builder.AddSigningCredential(primaryCertificate);
+                return;
+            }
 
+            if (TryFindCertificate("CertificateThumbprintSecondary", out X509Certificate2? secondaryCertificate))
+            {
+                builder.AddSigningCredential(secondaryCertificate);
+                return;
+            }
+
+            throw new Exception("Could not find a vali certificate matching the provided thumbprint");
         }
 
-        private static bool TryFindCertificate(string thumbprint, out X509Certificate2 certificate)
+        private static bool TryFindCertificate(string thumbprint, out X509Certificate2? certificate)
         {
             certificate = null;
-            return true;
+
+            if(string.IsNullOrEmpty(thumbprint))
+                return false;
+
+            using(var store = new X509Store(StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                var certs = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+
+                if(certs.Count == 0)
+                    return false;
+
+                var cert = certs.OfType<X509Certificate2>().SingleOrDefault();
+
+                if(cert == null)
+                    return false;
+
+                if(cert.NotAfter > DateTime.Now)
+                {
+                    certificate = cert;
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
